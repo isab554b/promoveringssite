3550f86bf5a1911c7706eeee55c8967ce3dd5cb4
“isab554b” <isab554b@stud.kea.dk>
2022-03-03 12:54:01 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      buttercream: "#F6E0A6",
+      apricot: "#F28B00",
+      peony: "#FAB6C5",
+      white: "#FFFFFF",
+      black: "#000000",
+    },
+
+    fontFamily: {
+      montserrat: "'Montserrat', sans-serif",
+    },
+    extend: {
+      backgroundImage: {
+        hero: "url('/img/hero.png')",
+        heromobil: "url('/img/heromobil.png')",
+        baggrund: "url('/img/baggrund.png')",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
